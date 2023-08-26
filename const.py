cd337fa983bcba5f514e6eb0c7808e651db55478
ManHinnn0509 <79153615+ManHinnn0509@users.noreply.github.com>
2023-08-26 12:34:18 +0800

    Change setting from .env to Python file (const.py)

--- const.py
+++ const.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-API_SECRET = os.getenv('API_SECRET')
+API_SECRET = os.getenv('API_SECRET', 'my-bg3-api')
 
-HOST_ADDRESS = os.getenv('HOST_ADDRESS')
-HOST_PORT = int(os.getenv('HOST_PORT'))
+HOST_ADDRESS = os.getenv('HOST_ADDRESS', '127.0.0.1')
+HOST_PORT = int(os.getenv('HOST_PORT', 1053))
